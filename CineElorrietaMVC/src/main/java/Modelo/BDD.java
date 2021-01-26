2d33742ee42759a411ebd286084e661703e8aa3f
in1dam <in1dam@elorrieta.int>
2021-01-26 12:03:19 +0100

    Conexión a BDD

---
+++ CineElorrietaMVC/src/main/java/Modelo/BDD.java
@@ -0,0 +1,26 @@
+package Modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class BDD {
+	
+	public static void conexion() {
+		
+		String url="jdbc:mysql://localhost:3306/reto3";
+		String username="dam";
+		String password="elorrieta";
+
+
+
+		try{
+
+		Connection conexionbd = DriverManager.getConnection(url,username,password);
+			System.out.println("Conectado correctamente.");
+		}catch(SQLException e){
+
+			System.out.println("No se ha podido conectar a la base de datos");
+			e.printStackTrace();
+		}
+	}
+
+}
