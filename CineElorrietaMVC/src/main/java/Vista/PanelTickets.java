6e80981b680d768d56f16ea923fac24685c27040
in1dam <in1dam@elorrieta.int>
2021-01-26 10:13:22 +0100

    Ticket desarrollo

--- CineElorrietaMVC/src/main/java/Vista/PanelTickets.java
+++ CineElorrietaMVC/src/main/java/Vista/PanelTickets.java
@@ -2,6 +2,8 @@ package Vista;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.AbstractListModel;
 import javax.swing.JButton;
@@ -16,6 +18,9 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.JMenuBar;
 import javax.swing.JTable;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.DefaultListModel;
 
 @SuppressWarnings("serial")
 public class PanelTickets extends JPanel {
@@ -24,30 +29,37 @@ public class PanelTickets extends JPanel {
 	private JLabel lblTickets;
 	private int contP1 = 0;
 	private int contP2 = 100;
-	private String[] texto2 =new String[256];
 
 	private ControladorPanelTickets controladorPanelTickets;
 	
 	public PanelTickets(ControladorPanelTickets controladorPanelTickets)
 	{
 		this.controladorPanelTickets = controladorPanelTickets;
-		
 		setLayout(null);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(263, 52, 131, 138);
-		add(textPane);
 		
 		lblTickets = new JLabel("Tickets");
 		lblTickets.setBounds(198, 15, 115, 14);
 		add(lblTickets);
 		
+		JList list_1_1 = new JList();
+		list_1_1.setModel(new AbstractListModel() {
+			String[] values = new String[] {};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list_1_1.setBounds(260, 52, 155, 138);
+		add(list_1_1);
 		
 		btnVolver = new JButton("Volver");
 		btnVolver.setBounds(34, 239, 89, 23);
 		add(btnVolver);
 		
 		JList list_1 = new JList();
+		list_1.setBounds(34, 52, 155, 138);
 		list_1.setModel(new AbstractListModel() {
 			String[] values = new String[] {"producto 1", "producto 2", "producto 3"};
 			public int getSize() {
@@ -57,29 +69,50 @@ public class PanelTickets extends JPanel {
 				return values[index];
 			}
 		});
-		list_1.setBounds(34, 52, 155, 138);
 		add(list_1);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerNumberModel(0, 0, 100, 1));
 		spinner.setBounds(199, 59, 41, 23);
+		spinner.setModel(new SpinnerNumberModel(0, 0, 100, 1));
 		add(spinner);
 		
 		JButton btnNewButton = new JButton("Añadir");
+		btnNewButton.setBounds(100, 205, 89, 23);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//Nombre del producto
 				String selec = list_1.getSelectedValue().toString();
-				int[] posicion = list_1.getSelectedIndices();
+				
 				Integer valor = (Integer)spinner.getValue();
 				String numero = String.valueOf(valor);
+				String numeros=numero.concat(numero);
 				String texto = selec+": Cant: "+numero+"\n";
-				texto2[variable] = texto;
-				String textoF = String.valueOf(texto2);
-				textPane.setText(textoF);
+				
+				
+				JList list_1_1 = new JList();
+				DefaultListModel modelo = new DefaultListModel();
+				modelo.addElement(texto);
+				list_1_1.setModel(modelo);
+				
+				
+			}
+
+		
+		});
+		add(btnNewButton);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String selec = list_1_1.getSelectedValue().toString();
+				
+				
 			}
 		});
-		btnNewButton.setBounds(100, 205, 89, 23);
-		add(btnNewButton);
+		btnEliminar.setBounds(270, 201, 89, 23);
+		add(btnEliminar);
+		
+		
 		
 		
 		
