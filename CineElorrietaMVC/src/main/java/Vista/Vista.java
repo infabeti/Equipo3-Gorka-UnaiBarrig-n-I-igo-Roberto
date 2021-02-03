b96b78d35dd8e0de135898081c3868830ced8c78
in1dam <in1dam@elorrieta.int>
2021-02-03 10:45:12 +0100

    Vista tamaño panel

--- CineElorrietaMVC/src/main/java/Vista/Vista.java
+++ CineElorrietaMVC/src/main/java/Vista/Vista.java
@@ -13,35 +13,14 @@ public class Vista extends JFrame{
 
 	public Vista() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(450, 150, 450, 300);
+		setBounds(450, 150, 501, 500);
 		getContentPane().setLayout(null);
 		
 		this.setTitle("RETO 3");
 		
-		JPanel panel = new JPanel();
-		
-		panel.setBounds(0, 0, 114, 261);
-		getContentPane().add(panel);
-		
 		Color cpanel = new Color(96,111,130);
 		Color cpanel2 = new Color(67,75,86);
 		Color cpanel3 = new Color(224,224,216);
-		
-		
-		
-		JPanel panel2 = new JPanel();
-		panel2.setBounds(114, 0, 320, 45);
-		getContentPane().add(panel2);
-		
-		
-		JPanel panel3 = new JPanel();
-		panel3.setBounds(114, 45, 320, 216);
-		getContentPane().add(panel3);
-		
-		
-		panel2.setBackground(cpanel);
-		panel3.setBackground(cpanel3);
-		panel.setBackground(cpanel2);
 		
 		
 		
